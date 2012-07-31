73604d5b7bad8622704578ee77bbe36b2c911f8d
doabit <doinsist@gmail.com>
2012-07-31 22:20:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+activeadmin_mongoid_filter_demo
+===============================
+
+activeadmin_mongoid_filter_demo
+
+email:    admin@demo.com
+passowrd: abc123
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveadminMongoidFilterDemo::Application.config.secret_token = '33484a5a2d7677c57592ca1349b470f7589e367302a64859681c27e40c5adcbc97243f2722a3f1782665b30e42a090f022adbb6ba320c265702c044ec1de9fd4'
